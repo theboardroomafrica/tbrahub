ede269941e2afbbc9e41bc088c220eb104671be7
Julius A. Odai <jlsodai@gmail.com>
2024-08-02 12:28:59 +0000

    add status actions

--- app/Models/Application.php
+++ app/Models/Application.php
@@ -10,4 +10,25 @@ class Application extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    public function approve()
+    {
+        return $this->update(['status' => 1]);
+    }
+
+    public function markAsPending()
+    {
+        return $this->update(['status' => null]);
+    }
+
+    public function reject()
+    {
+        return $this->update(['status' => 0]);
+    }
+
+    public function getStatusTextAttribute()
+    {
+        if (is_null($this->status)) return "Pending";
+        return $this->status ? "Approved" : "Rejected";
+    }
 }

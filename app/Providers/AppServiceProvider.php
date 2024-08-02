99654f82f05848d8cbca75a5592898509669495f
Julius A. Odai <jlsodai@gmail.com>
2024-08-02 11:03:08 +0000

    add custom filament

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Filament\Support\Assets\Css;
+use Filament\Support\Facades\FilamentAsset;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        FilamentAsset::register([
+            Css::make('custom-filament', public_path('css/app/custom-filament.css'))
+        ]);
     }
 }

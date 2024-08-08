30da71ac2ae3e30db82a5d76bfaf2f6e237fd027
Julius A. Odai <jlsodai@gmail.com>
2024-08-08 10:20:16 +0000

    add add-application api route

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/add-application', function (Request $request) {
+    $application = App\Models\Application::create($request->all());
+    // \Illuminate\Support\Facades\Log::info('Request Body:', $request->all());
+    return response()->json(["status" => "success", "application" => $application], 200);
+});

c6c0d1113d563cd480d5821f1d9bf9d8ed7f1568
Julius A. Odai <jlsodai@gmail.com>
2024-08-15 12:43:19 +0000

    make experience manager universal

--- app/Livewire/ExperienceManager.php
+++ app/Livewire/ExperienceManager.php
@@ -3,8 +3,10 @@
 namespace App\Livewire;
 
 use App\Filament\Utilities\FormUtility;
+use App\Models\Achievement;
 use App\Models\BoardExperience;
 use App\Models\ProfessionalExperience;
+use App\Models\UserLanguage;
 use Filament\Actions\Action;
 use Filament\Actions\Concerns\InteractsWithActions;
 use Filament\Actions\Contracts\HasActions;
@@ -19,7 +21,7 @@ class ExperienceManager extends Component implements HasForms, HasActions
     use InteractsWithForms;
     use InteractsWithActions;
 
-    public $experiences;
+    public $records;
     public $actions = false;
     public $model = "professional";
     public $title = "Professional Experience";
@@ -27,19 +29,22 @@ class ExperienceManager extends Component implements HasForms, HasActions
 
     public function mount()
     {
-        $this->experiences = $this->model == 'professional' ?
-            $this->user->orderedProfessionalExperiences() :
-            $this->user->orderedBoardExperiences();
+        $this->records = $this->modelOptions()[$this->model]["records"];
     }
 
     public function getModel()
     {
-        return $this->model == "professional" ? ProfessionalExperience::class : BoardExperience::class;
+        return $this->modelOptions()[$this->model]["model"];
+    }
+
+    public function getWidth()
+    {
+        return $this->modelOptions()[$this->model]["width"] ?? MaxWidth::SixExtraLarge;
     }
 
     public function getFormUtility()
     {
-        return $this->model == "professional" ? FormUtility::ProfersionalExperience() : FormUtility::BoardExperience();
+        return $this->modelOptions()[$this->model]["form"];
     }
 
     public function editAction(): Action
@@ -49,7 +54,7 @@ class ExperienceManager extends Component implements HasForms, HasActions
                 ...$this->getFormUtility()
             ])
             ->color('info')
-            ->modalWidth(MaxWidth::SixExtraLarge)
+            ->modalWidth($this->getWidth())
             ->iconButton()
             ->icon('heroicon-o-pencil')
             ->record(function ($arguments) {
@@ -65,11 +70,14 @@ class ExperienceManager extends Component implements HasForms, HasActions
                 ...$this->getFormUtility()
             ])
             ->iconButton()
+            ->modalWidth($this->getWidth())
             ->extraAttributes(['class' => 'btn-tender text-white hover:text-white'])
-            ->modalHeading('Add Professional Experience')
+            ->modalHeading('Add ' . $this->title)
             ->action(function ($data) {
                 $user = auth()->user();
                 $this->getModel()::create(['user_id' => $user->id, ...$data]);
+
+                $this->emit('refresh');
             })
             ->icon('heroicon-o-plus');
     }
@@ -87,5 +95,33 @@ class ExperienceManager extends Component implements HasForms, HasActions
     public function render()
     {
         return view('livewire.experience-manager');
+    }
+
+    private function modelOptions()
+    {
+        return [
+            "professional" => [
+                "records" => $this->user->orderedProfessionalExperiences(),
+                "model" => ProfessionalExperience::class,
+                "form" => FormUtility::ProfersionalExperience()
+            ],
+            "board" => [
+                "records" => $this->user->orderedBoardExperiences(),
+                "model" => BoardExperience::class,
+                "form" => FormUtility::BoardExperience()
+            ],
+            "achievement" => [
+                "records" => $this->user->achievements,
+                "model" => Achievement::class,
+                "form" => FormUtility::achievements(),
+                "width" => MaxWidth::ExtraLarge
+            ],
+            "language" => [
+                "records" => $this->user->languages,
+                "model" => UserLanguage::class,
+                "form" => FormUtility::Languages(),
+                "width" => MaxWidth::Large
+            ],
+        ];
     }
 }

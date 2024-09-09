3a65ce371c794e1f0c3334c7c6de657d8c13f84d
Julius A. Odai <jlsodai@gmail.com>
2024-09-09 10:22:18 +0000

    redirect to search index after create

--- app/Filament/Client/Resources/SearchResource/Pages/CreateSearch.php
+++ app/Filament/Client/Resources/SearchResource/Pages/CreateSearch.php
@@ -3,10 +3,14 @@
 namespace App\Filament\Client\Resources\SearchResource\Pages;
 
 use App\Filament\Client\Resources\SearchResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateSearch extends CreateRecord
 {
     protected static string $resource = SearchResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }

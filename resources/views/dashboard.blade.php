6572fd80e2e4733735fdb028e05c419c1b500f14
Julius A. Odai <jlsodai@gmail.com>
2024-11-27 08:47:32 +0100

    finalise member dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,7 +5,7 @@
     <section>
         <div class="container grid grid-cols-[2fr_1fr] gap-8">
             <div class="mainbar">
-                <div class="card">
+                <div class="card2">
                     <div class="flex justify-between mb-4">
                         <h3 class="font-bold text-md">
                             New Opportunities
@@ -43,7 +43,7 @@
                     </div>
                 </div>
 
-                <div class="card mt-8">
+                <div class="card2 mt-8">
                     <div class="flex justify-between">
                         <h3 class="font-bold text-md">
                             Events and programmes
@@ -51,10 +51,10 @@
                         <a href="#" class=""> → All Events / Programmes</a>
                     </div>
                     <div class="opportunities grid grid-cols-2 gap-8 mt-4">
-                        <div class="border border-gray-200 rounded-lg overflow-hidden bg-white">
-                            <img
-                                src="https://res.cloudinary.com/dhhw72iwq/image/upload/v1715676734/WebinarBnnr_tgsrkk.jpg"
-                                class="mx-auto h-[200px] object-cover"/>
+                        <a href="#" class="border border-gray-200 rounded-lg overflow-hidden bg-white">
+                            <img alt=""
+                                 src="https://res.cloudinary.com/dhhw72iwq/image/upload/v1715676734/WebinarBnnr_tgsrkk.jpg"
+                                 class="mx-auto h-[200px] object-cover"/>
                             <div class="p-4 flex gap-3 items-center">
                                 <div class="bg-mustard-200 text-sm py-1 px-3 rounded flex flex-col">
                                     <p class="text-mustard-600 font-bold text-xs">JUL</p>
@@ -65,11 +65,11 @@
                                     <p class="text-sm"><b>Coach:</b> Rachel Adams</p>
                                 </div>
                             </div>
-                        </div>
-                        <div class="border border-gray-200 rounded-lg overflow-hidden bg-white">
-                            <img
-                                src="https://res.cloudinary.com/dhhw72iwq/image/upload/v1720431441/SteveWebinerBG_vnpvbd.jpg"
-                                class="mx-auto h-[200px] object-cover"/>
+                        </a>
+                        <a href="#" class="border border-gray-200 rounded-lg overflow-hidden bg-white">
+                            <img alt=""
+                                 src="https://res.cloudinary.com/dhhw72iwq/image/upload/v1720431441/SteveWebinerBG_vnpvbd.jpg"
+                                 class="mx-auto h-[200px] object-cover"/>
                             <div class="p-4 flex gap-3 items-center">
                                 <div class="bg-mustard-200 text-sm py-1 px-3 rounded flex flex-col">
                                     <p class="text-mustard-600 font-bold text-xs">JUL</p>
@@ -80,29 +80,31 @@
                                     <p class="text-sm"><b>Coach:</b> Rachel Adams</p>
                                 </div>
                             </div>
-                        </div>
+                        </a>
                     </div>
                 </div>
             </div>
 
             <div class="sidebar card self-start">
                 <h3 class="font-bold mb-4 text-md">Professional Support</h3>
-                <img src="https://placehold.co/400x470" alt="" class="rounded-md"/>
-                <img src="https://placehold.co/400x150" alt="" class="rounded-md mt-8"/>
+                <img src="https://res.cloudinary.com/dhhw72iwq/image/upload/v1732675512/NextInterview_poflst.jpg" alt=""
+                     class="rounded-md"/>
             </div>
         </div>
     </section>
 
     <section>
         <div class="container">
-            <div class="rounded-lg border mt-8 py-12 px-16 flex justify-between bg-white">
+            <div class="card2 mt-8 py-12 px-16 flex justify-between">
                 <div class="">
                     <h2 class="text-3xl font-bold">Connect on the go</h2>
                     <p class="text-xl">Download TheBoardroom Africa mobile app</p>
                 </div>
                 <div class="flex gap-4">
-                    <img src="https://placehold.co/200x70">
-                    <img src="https://placehold.co/200x70">
+                    <img class="h-[70px]"
+                         src="https://res.cloudinary.com/dhhw72iwq/image/upload/v1732676101/play_f4zwjc.svg">
+                    <img class="h-[70px]"
+                         src="https://res.cloudinary.com/dhhw72iwq/image/upload/v1732676101/app_vudinv.svg">
                 </div>
             </div>
         </div>

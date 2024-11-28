db9caa597c5a5eee2c1f453dadec4f526aad0a18
Julius A. Odai <jlsodai@gmail.com>
2024-11-28 13:21:15 +0100

    add some transition

--- resources/views/components/menu-item.blade.php
+++ resources/views/components/menu-item.blade.php
@@ -4,6 +4,6 @@
         class="flex items-center p-2 rounded-lg transition duration-75 hover:bg-gray-100 group"
     >
         <x-dynamic-component :component="'heroicon-o-' . $icon" class="h-6 w-6"/>
-        <span class="ml-3 hidden">{{ $title }}</span>
+        <span class="ml-3" :class="toggleSidebar ? 'hidden' : ''">{{ $title }}</span>
     </a>
 </li>
--- resources/views/layouts/dash.blade.php
+++ resources/views/layouts/dash.blade.php
@@ -61,7 +61,7 @@
         <!-- Sidebar -->
 
         <aside
-            class="fixed top-0 left-0 z-40 h-screen pt-14 transition-transform -translate-x-full bg-white border-r border-gray-200 md:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
+            class="fixed top-0 left-0 z-40 h-screen pt-14 -translate-x-full bg-white border-r border-gray-200 md:translate-x-0 dark:bg-gray-800 dark:border-gray-700 transition-all duration-300 ease-in-out"
             :class="toggleSidebar ? 'w-16' : 'w-64'"
             aria-label="Sidenav"
             id="drawer-navigation"

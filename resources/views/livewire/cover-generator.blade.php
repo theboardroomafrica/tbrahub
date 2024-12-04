371b1051808d2aa569be13ee068f304fa5a64b27
Julius A. Odai <jlsodai@gmail.com>
2024-12-04 15:00:49 +0100

    update cover generator button; fix spacing on apply page

--- resources/views/livewire/cover-generator.blade.php
+++ resources/views/livewire/cover-generator.blade.php
@@ -1,9 +1,15 @@
 <div>
-    <button wire:click="getResponse">Generate CV</button>
 
-    <label for="cvgenerator" class="block mb-2 text-sm font-medium text-gray-900 mt-8">Cover Letter</label>
+    <div class="flex items-center justify-between my-2">
+        <label for="cvgenerator" class="block text-sm font-medium text-gray-900">Cover
+            Letter</label>
+        <button wire:click="getResponse" class="flex btn btn-white gap-2 items-center">
+            <x-heroicon-o-sparkles class="h-6 w-6"/>
+            Generate cover letter
+        </button>
+    </div>
     <textarea wire:stream="stream" id="cvgenerator" rows="16"
-              class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300"
-              placeholder="Write your thoughts here...">{{ $response }}</textarea>
+              class="block p-4 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300"
+              placeholder="Write your cover letter here or use the AI generator...">{{ $response }}</textarea>
 
 </div>
--- resources/views/opportunities/apply.blade.php
+++ resources/views/opportunities/apply.blade.php
@@ -1,10 +1,9 @@
 <x-dash-layout>
-
     <section class="pt-12">
         <x-opportunity-header :opportunity="$opportunity" :hideApply="true"/>
 
         <div class="container">
-            <div class="card2 rounded-t-none border-t-0">
+            <div class="card2 rounded-t-none border-t-0 pb-16">
                 <div class="max-w-4xl mt-8 mx-auto">
                     <livewire:cover-generator/>
                 </div>

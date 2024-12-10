f9fd3f0aa1432c2a170b8a7b9c8ac812db2f952a
Julius A. Odai <jlsodai@gmail.com>
2024-12-10 17:39:58 +0100

    work on cover letter... TODO: improve

--- app/Livewire/CoverGenerator.php
+++ app/Livewire/CoverGenerator.php
@@ -8,11 +8,18 @@ use OpenAI\Laravel\Facades\OpenAI;
 class CoverGenerator extends Component
 {
     public ?string $response = null;
+    public string $initialPlaceholder = "Write your cover letter here or use the AI generator...";
+    public string $placeholder = "Write your cover letter here or use the AI generator...";
 
     public function getResponse()
     {
+        $this->response = '';
+
+        $this->placeholder = "Generating cover letter...";
+
         $stream = OpenAI::threads()->createAndRunStreamed([
-            'assistant_id' => 'asst_FJ8ymye160lifo7BXayuGDJC',
+            'assistant_id' => 'asst_METMOpNm0R2mRDGgWn4MQOCI',
+            'model' => 'gpt-4o-mini',
             'thread' => [
                 'messages' =>
                     [
@@ -33,6 +40,8 @@ class CoverGenerator extends Component
 
             $this->stream('stream', $content);
         }
+
+        $this->placeholder = "Write your cover letter here or use the AI generator...";
     }
 
     public function render()
--- resources/views/livewire/cover-generator.blade.php
+++ resources/views/livewire/cover-generator.blade.php
@@ -10,6 +10,6 @@
     </div>
     <textarea wire:stream="stream" id="cvgenerator" rows="16"
               class="block p-4 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300"
-              placeholder="Write your cover letter here or use the AI generator...">{{ $response }}</textarea>
+              placeholder="{{ $placeholder }}">{{ $response }}</textarea>
 
 </div>

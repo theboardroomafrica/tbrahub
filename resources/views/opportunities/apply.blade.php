6b89231a13c4e984b0c30ec369bc58535662e8de
Julius A. Odai <jlsodai@gmail.com>
2024-12-13 04:03:31 +0100

    add role application title

--- resources/views/opportunities/apply.blade.php
+++ resources/views/opportunities/apply.blade.php
@@ -5,6 +5,7 @@
         <div class="container">
             <div class="card2 rounded-t-none border-t-0 pb-16">
                 <div class="max-w-4xl mt-8 mx-auto">
+                    <h2 class="mb-4 text-xl font-semibold">Role application</h2>
                     <livewire:opportunity-application-form :opportunity="$opportunity" :record="$application"/>
                 </div>
             </div>
